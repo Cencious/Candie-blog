fc05c3bc38c473db290b1473323cc82d69bb16b5
Cencious <user.email>
2022-05-15 18:20:38 +0300

    create a class for registration

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,2 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField 
+from wtforms.validators import Required,Email
+
+
+class RegistrationForm(FlaskForm):
+    
