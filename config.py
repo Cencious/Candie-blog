f5bb6d444d5abe1efb1d311a08d34d4dd327d0b1
Cencious <user.email>
2022-05-15 17:45:47 +0300

    add squlachemy database uri

--- config.py
+++ config.py
@@ -5,3 +5,6 @@ class Config:
     General configuration parent class
     '''
     QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kakan:Abiathar2022@localhost/blog'
+
