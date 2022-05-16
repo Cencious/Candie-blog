570cf2e2a05a5b29ccccd7cb1cf2428df7de4fb2
Cencious <user.email>
2022-05-16 04:24:44 +0300

    Authoniticate registration route

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,3 +5,15 @@ from .. import db
 from . import auth
 from ..email import mail_message
 from flask_login import login_user,logout_user,login_required, current_user
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        mail_message("Welcome to Candie Blog","email/welcome_user",user.email,user=user)
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form=form)
