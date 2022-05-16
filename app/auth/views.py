6c384ebd45cfe356f585e940a269ebfd8a2a66f1
Cencious <user.email>
2022-05-16 04:29:04 +0300

    Authenticate logout route

--- app/auth/views.py
+++ app/auth/views.py
@@ -32,3 +32,9 @@ def login():
     title = "Candie Blog login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
+
