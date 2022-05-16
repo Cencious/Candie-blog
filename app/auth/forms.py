5ce274d19bcb9b4217a9f5864895efd120278c4a
Cencious <user.email>
2022-05-16 04:13:31 +0300

    add password confirm field

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField 
-from wtforms.validators import Required,Email
+from wtforms.validators import DataRequired,Email,EqualTo
+from ..models import User
+from wtforms import ValidationError
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
 
 
 class RegistrationForm(FlaskForm):
-    
+    email = StringField('Your Email Address',validators=[DataRequired(),Email()])
+    username = StringField('Enter your username',validators = [DataRequired()])
+    password = PasswordField('Password',validators = [DataRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [DataRequired()])
+    submit = SubmitField('Sign Up')
