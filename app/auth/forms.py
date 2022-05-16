1ce363bc060b35dcddfde9221362acb2b2ec5615
Cencious <user.email>
2022-05-16 04:15:14 +0300

    field for email validation

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -12,3 +12,7 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password',validators = [DataRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [DataRequired()])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('An account with that email already exists.')
