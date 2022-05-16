264632db879cfecf3d0dd01801906401726ca2bb
Cencious <user.email>
2022-05-16 04:40:23 +0300

    add update profile class for users

--- app/main/form.py
+++ app/main/form.py
@@ -8,3 +8,7 @@ class BlogForm(FlaskForm):
     title = StringField('Author', validators=[DataRequired()])
     post = TextAreaField('Blog', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Let us know you better',validators = [DataRequired()])
+    submit = SubmitField('Submit')
