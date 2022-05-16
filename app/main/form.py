b66a9c502e52d3e5c6851350a9d01ac87178e014
Cencious <user.email>
2022-05-16 04:39:20 +0300

    add class BlogForm,with title, post and submit fields

--- app/main/form.py
+++ app/main/form.py
@@ -1,3 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField, SelectField
 from wtforms.validators import DataRequired, Email
+
+
+class BlogForm(FlaskForm):
+
+    title = StringField('Author', validators=[DataRequired()])
+    post = TextAreaField('Blog', validators=[DataRequired()])
+    submit = SubmitField('Post')
